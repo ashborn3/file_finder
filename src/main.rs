f6494259cb694d15563289acd570a210634f362e
ashborn3 <nitin.dev.ashborn3@gmail.com>
2023-10-31 13:28:49 +0530

    Added proper way to handle a missing cache.bin

--- src/main.rs
+++ src/main.rs
@@ -4,37 +4,49 @@ use std::env;
 
 mod hashmap;
 mod cachehash;
+mod benchmarkcollisions;
 
 fn main() {
+    //Parse argument for file name
     let search_string = match env::args().nth(1) {
         Some(value) => value,
         None => panic!("No Argument Given")
     };
 
-    let init_path = "C:/";
+    let init_path = "C:\\";
+    let mut fs_hash_map: HashMap<u64, Vec<String>> = HashMap::new();
 
-    let formatted_path = format_string_for_fs(init_path);
+    //Make new cache.bin if it doesn't exist
+    if !(std::path::Path::new("cache.bin").exists()) {
+        println!("cache.bin not found\nRemaking cache.bin");
+        let nowa: Instant = Instant::now();
+        hashmap::hash_map_of_target_location(&mut fs_hash_map, init_path.to_string());
+        let cache = cachehash::cache { map: fs_hash_map };
+        cachehash::cache_the_hash(&cache);
+        let nowb: std::time::Duration = nowa.elapsed();
+        println!("Remaking cache.bin took {:?}", nowb);
+    }
 
-    let now_read_hash_from_cache = Instant::now();
-    
     // 2.34% collision percentage benchmarked on C drive
     // hashmap::test_hash_key_collisions(formatted_path);
 
-    let mut fs_hash_map: HashMap<u64, Vec<String>> = HashMap::new();
-
+    //Load the HashMap from cache.bin
+    println!("Loading cache.bin into memory");
+    let now_read_hash_from_cache: Instant = Instant::now();
     fs_hash_map = cachehash::get_hash_from_cache();
-
-    let time_taken_to_read_hash_from_cache = now_read_hash_from_cache.elapsed();
+    let time_taken_to_read_hash_from_cache: std::time::Duration = now_read_hash_from_cache.elapsed();
 
     println!("Reading HashMap from cache.bin took {:?}", time_taken_to_read_hash_from_cache);
 
-    let now1 = Instant::now();
+    //Search for the file
+    println!("Searching for {}", search_string);
+    let now1: Instant = Instant::now();
     hashmap::hash_map_get_path(&fs_hash_map, hashmap::hash_path(&search_string));
-
-    let now2 = now1.elapsed();
+    let now2: std::time::Duration = now1.elapsed();
 
     println!("Took {:?} to Search for {}", now2, search_string);
 
+    // CODE NO WORK, WHY ?
     // while true {
     //     print!("Enter your name : ");
         
@@ -49,15 +61,17 @@ fn main() {
     // }
 }
 
-fn format_string_for_fs(str: &str) -> String {
-    let mut final_str = String::new();
-    for ch in str.chars() {
-        if ch == '/' {
-            final_str += "\\";
-        }
-        else {
-            final_str.push(ch);
-        }
-    }
-    final_str
-}
+
+// WHY DID I WRITE THIS ?
+// fn format_string_for_fs(str: &str) -> String {
+//     let mut final_str = String::new();
+//     for ch in str.chars() {
+//         if ch == '/' {
+//             final_str += "\\";
+//         }
+//         else {
+//             final_str.push(ch);
+//         }
+//     }
+//     final_str
+// }
